d431f858502b3f5ffde662a28b9a762c6f747c85
Lucas Henrique <lucasti@live.com>
2023-02-15 07:40:11 -0300

    api now receive teamembers options

--- src/infrastructure/validations/pokeTeam.validation.ts
+++ src/infrastructure/validations/pokeTeam.validation.ts
@@ -1,8 +1,16 @@
 import Joi from "joi"
 
 export const PokedexPostSchema = Joi.object({
-  id:Joi.number().optional(),
+  id: Joi.number().optional(),
   timeName: Joi.string().required(),
   playerName: Joi.string().required(),
-  teamMembers: Joi.array().items(Joi.string()).required(),
+  teamMembers: Joi.array()
+    .items(
+      Joi.object({
+        name: Joi.string().required(),
+        id: Joi.number().required(),
+        avatarUrl: Joi.string().required(),
+      }),
+    )
+    .required(),
 }).unknown(false)
